6e7874c7cd07650d9a25088f925d5d91b2986fcd
kcs5540 <csjeju21@gmail.com>
2026-07-29 01:40:55 +0900

    Use CommonJS for api/save-consultation.js and configure clean routing in vercel.json

--- api/save-consultation.js
+++ api/save-consultation.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -21,4 +21,4 @@ export default function handler(req, res) {
   }
   
   res.status(405).json({ error: 'Method not allowed' });
-}
+};
